11d47775a7b3f2a77246c10b2d76e8ac84e32570
pradip292 <pradipwarkhade74@gmail.com>
2024-08-21 10:50:05 +0530

    CA

---
+++ Practice/src/Accenture/filenameascending.java
@@ -0,0 +1,50 @@
+package Accenture;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class filenameascending {
+    public static void main(String[] args){
+        /*
+        String[] arr= {"file","name","mouse"}; //fmn
+//        ArrayList<Character> ca= new ArrayList<>();
+        StringBuilder str= new StringBuilder();
+        int m=0;
+        for(int i=0; i<arr.length; i++){
+            String ch = arr[m];
+            char ch1= ch.charAt(0);
+            str.append(ch1);
+            m++;
+        }
+        ArrayList<Character> al = new ArrayList<>();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            al.add(ch);
+        }
+        Collections.sort(al);
+        System.out.println(al);
+        System.out.println(al.toString());
+
+        StringBuilder strf= new StringBuilder();
+        for(int i=0; i<al.size(); i++){
+            strf.append(al.get(i));
+        }
+        System.out.println(strf);
+         */
+
+
+        String s = ""; // nfe
+        String[] arr={"mile","leen", "zango"};
+        for (int i=0; i<arr.length; i++){
+            char c=arr[i].charAt(0);
+            s+=c;
+        }
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars); // e,f,n
+        String sortedString = new String(chars); // efn
+        System.out.println(sortedString);
+    }
+}
+
---
+++ Practice/src/MostAsked/googly_prime.java
@@ -0,0 +1,40 @@
+package MostAsked;
+
+public class googly_prime { //psvm
+
+    public static boolean calprime(int n){
+        for(int i=2; i<Math.sqrt(n); i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int num=113;
+//        if(num==0){
+//            System.out.println("True");
+//        }
+//        if(num==1 || num ==2){
+//            System.out.println("TURU");
+//        }
+        if(calprime(num)){
+            int sum=0;
+            while(num>0){
+                int ld =num%10;
+                sum+=ld;
+                num=num/10;
+            }
+            if(calprime(sum)){
+                System.out.println("TRUE");
+            }
+            else {
+                System.out.println("FALSE");
+            }
+        }
+        else {
+            System.out.println("FALSE");
+        }
+
+    }
+}
