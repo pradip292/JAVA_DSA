e7576989d84944799404484282aa43d341b9f379
pradip292 <pradipwarkhade74@gmail.com>
2024-08-23 14:56:27 +0530

    ca

--- Practice/src/TalentBattle/LeapYear.java
+++ Practice/src/TalentBattle/LeapYear.java
@@ -3,7 +3,10 @@ package TalentBattle;
 public class LeapYear {
     public static boolean calleapyear(int num){
         int flag=0;
-        if(num%4==0)
+        if(num==0){
+            return false;
+        }
+        else if(num%4==0)
         {
             flag=1;
             if(num%100==0)
@@ -28,7 +31,7 @@ public class LeapYear {
         }
     }
     public static void main(String[] args) {
-        int year = 1620;
+        int year = 0;
         System.out.println(calleapyear(year));
     }
 }
